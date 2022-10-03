0f346b6c1c1726285698e64af069c036dcfb96b6
Joel Jolly <joeljolly101@gmail.com>
2022-10-03 12:39:05 +0100

    excerise 1.4 complete

--- primes.py
+++ primes.py
@@ -2,5 +2,25 @@
 """ENTER YOUR SOLUTION HERE!"""
 
 def primes(number_of_primes):
-    list = []
-    return list
+    try:
+        list = []
+        counter = 2
+        while number_of_primes != len(list):
+            if checkPrime(counter):
+                list.append(counter)
+            counter +=1
+
+        print(list)
+    except ValueError as ve:
+        print("The value you entered is not a positive number!")
+
+def checkPrime(num):
+    if num == 2:
+        return True
+    else:
+        for i in range(2,num):
+            if (num%i == 0):
+                return False
+    return True
+
+primes(7)
